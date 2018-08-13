0175169d5502a9a40f3b90c8614afde633481e75
alynielson <nielsonaly@gmail.com>
2018-08-13 09:00:27 -0500

    User Interface Get First Name method

--- Sweepstakes/UserInterface.cs
+++ Sweepstakes/UserInterface.cs
@@ -8,5 +8,21 @@ namespace Sweepstakes
 {
     public static class UserInterface
     {
+        public static string GetFirstName()
+        {
+            string response = "";
+            do
+            {
+                Console.WriteLine("Enter your first name:");
+                response = Console.ReadLine();
+                Console.Clear();
+                if (response == "")
+                {
+                    Console.WriteLine("You didn't enter anything! Try again.");
+                }
+            }
+            while (response == "");
+            return response;
+        }
     }
 }
